02bc56b03bcb4279d040da7645a38307f8f960e1
Pedro <peemanuel68@gmail.com>
2026-07-13 13:27:14 -0300

    Update Analisador Climatico Inteligente.py

--- Analisador Climatico Inteligente.py
+++ Analisador Climatico Inteligente.py
@@ -1,1 +1,12 @@
+import requests
 
+def buscar_clima_por_nome():
+    print("\n--- CONSULTA DE CLIMA ---")
+    cidade = input("Digite o nome da cidade (ex: Fortaleza, São Paulo, Toronto): ").strip()
+    
+    if not cidade:
+        print("Você precisa digitar o nome de uma cidade!")
+        return
+
+    # Descobre as coordenadas (latitude e longitude) pelo nome da cidade
+    url_geocodificacao = f"https://geocoding-api.open-meteo.com/v1/search?name={cidade}&count=1&language=pt"
